00dd7421ca252f664c869d9077b3703201f0f0a9
Simone Rimedio <77270481+SimoneRimedio@users.noreply.github.com>
2022-11-09 11:17:17 +0100

    Update robottinoWifi(PcToPhone).ino

--- robottinoWifi(PcToPhone).ino
+++ robottinoWifi(PcToPhone).ino
@@ -1,1 +1,14 @@
-dssas
+#include <SoftwareSerial.h>
+//libreria che mi fa accedere alla classe SoftwareSerial
+//attributi: pinTx e pin Rx
+//metodi: begin(), available(), read(), print(), println()
+//begin() usato per configurare la velocità della porta seriale
+//print() e println() usati per inviare i dati da Arduino a modulo WIFI
+//available() usato per verificare presenza dati trasmessi da moduloWIFI per Arduino
+//read() usato per ricevere i dati provenienti da moduloWIFI
+
+#define rxPin 10
+#define txPin 8
+// Set up a new SoftwareSerial object
+SoftwareSerial serialModuloWIFI =  SoftwareSerial(rxPin, txPin);
+
